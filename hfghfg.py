34413a2a8d3a99b0c50238a92b5d436007deb26a
shilorya <doknes@mail.ru>
2023-11-18 11:54:14 +0300

    124324

---
+++ hfghfg.py
@@ -0,0 +1,34 @@
+# task 1(A)
+import random
+
+# z = [random.randint(0, 10) for a in range(8)]
+# print(z)
+# print(len(set(z)))
+
+# task 2(B)
+
+# z = set([random.randint(0, 10) for z in range(8)])
+# x = set([random.randint(0, 10) for x in range(8)])
+# print(z)
+# print(x)
+
+# count = 0
+# for i in z:
+#     for j in x:
+#         if i == j:
+#             count +=1
+# print(count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
